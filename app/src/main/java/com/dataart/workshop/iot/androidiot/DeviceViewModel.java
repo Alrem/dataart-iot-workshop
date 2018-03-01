09f8c912eb1345ba2e57f6733339b665966302c1
Eugene Dubovik <edubovik90@gmail.com>
2018-03-01 19:05:33 +0200

    Added commentaries;

--- app/src/main/java/com/dataart/workshop/iot/androidiot/DeviceViewModel.java
+++ app/src/main/java/com/dataart/workshop/iot/androidiot/DeviceViewModel.java
@@ -63,9 +63,12 @@ public class DeviceViewModel extends ViewModel {
                     return device.sendCommand(TEMPERATURE_COMMAND_NAME,
                             Collections.singletonList(parameter));
                 })
+                //Retry if Device didn't send result to server
                 .flatMap(this::getRetryForTemperature)
+                //Boolean key for repeat operation
                 .repeatUntil(supplier)
                 .compose(applySchedulers())
+                //Send temperature value to the UI
                 .subscribe(temperature::setValue, Throwable::printStackTrace);
         return temperature;
 
@@ -77,45 +80,59 @@ public class DeviceViewModel extends ViewModel {
     }
 
     public LiveData<Boolean> sendOffCommand(String id) {
-        Observable.just(id).map(deviceId -> deviceHive.getDevice(deviceId).getData())
+        Observable.just(id)
+                //This function is getting Device Object by id
+                .map(deviceId -> deviceHive.getDevice(deviceId).getData())
+                //This function is sending command to the Device that we got before
                 .map(device -> {
                     Parameter parameter = new Parameter(LED_PARAMETER_KEY, LED_TURN_OFF_PARAMETER_VALUE);
                     return device.sendCommand(LED_COMMAND_NAME, Collections.singletonList(parameter));
 
                 })
-                .flatMap(this::getRetry)
+                //Retry if Device didn't send status to server
+                .flatMap(this::getRetryForLED)
+                //Check if status is OK
                 .map(c -> c.equalsIgnoreCase(OK))
                 .compose(applySchedulers())
+                //Send temperature value to the UI
                 .subscribe(success -> ledStatusOff.setValue(success), Throwable::printStackTrace);
         return ledStatusOff;
     }
 
     public LiveData<Boolean> sendOnCommand(String id) {
         Observable.just(id)
+                //This function is getting Device Object by id
                 .map(deviceId -> deviceHive.getDevice(deviceId).getData())
+                //This function is sending command to the Device that we got before
                 .map(device -> {
                     Parameter parameter = new Parameter(LED_PARAMETER_KEY, LED_TURN_ON_PARAMETER_VALUE);
                     return device.sendCommand(LED_COMMAND_NAME, Collections.singletonList(parameter));
 
                 })
-                .flatMap(this::getRetry)
+                //Retry if Device didn't send status to server
+                .flatMap(this::getRetryForLED)
+                //Check if status is OK
                 .map(c -> c.equalsIgnoreCase(OK))
                 .compose(applySchedulers())
+                //Send temperature value to the UI
                 .subscribe(success -> ledStatusOn.setValue(success), Throwable::printStackTrace);
         return ledStatusOn;
     }
 
+    //Define threads where the logic will start and end
     private <T> ObservableTransformer<T, T> applySchedulers() {
         return observable -> observable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    private Observable<String> getRetry(DHResponse<DeviceCommand> deviceCommandDHResponse) {
+    //Fetch command status and retry if status is null
+    private Observable<String> getRetryForLED(DHResponse<DeviceCommand> deviceCommandDHResponse) {
         return Observable.just(deviceCommandDHResponse)
                 .map(cmd -> cmd.getData().fetchCommandStatus().getData())
                 .retryWhen(new RetryWithDelay(maxRetries, ledDelayInMillis));
     }
 
+    //Fetch command result and retry if result is null
     private Observable<Float> getRetryForTemperature(DHResponse<DeviceCommand> commandDHResponse) {
         return Observable.just(commandDHResponse)
                 .map(cmd -> cmd.getData().fetchCommandResult().getData().get(KEY_TEMPERATURE_RESULT).getAsFloat())
